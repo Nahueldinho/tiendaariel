487a9a288f4e3417669e10b1f5e7b96bc87fa6f1
bautibustos <47929345+bautibustos@users.noreply.github.com>
2023-09-01 13:10:20 -0300

    pene

--- tiendaariel/src/main/java/com/example/tiendaariel/clases/Usuario.java
+++ tiendaariel/src/main/java/com/example/tiendaariel/clases/Usuario.java
@@ -3,6 +3,7 @@ package com.example.tiendaariel.clases;
 public class Usuario {
     private int id;
     private String nombre;
+    private String apellido;
     private String usuario;
     private String contraseña;
     private String correo;
@@ -10,9 +11,10 @@ public class Usuario {
     public Usuario() {
     }
 
-    public Usuario(int id, String nombre, String usuario, String contraseña, String correo) {
+    public Usuario(int id, String nombre, String apellido, String usuario, String contraseña, String correo) {
         this.id = id;
         this.nombre = nombre;
+        this.apellido = apellido;
         this.usuario = usuario;
         this.contraseña = contraseña;
         this.correo = correo;
@@ -32,6 +34,14 @@ public class Usuario {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
 
     public String getUsuario() {
